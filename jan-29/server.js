9ac2b010c2af792f52205d12899646b76b178ac8
Yash-upadhyay1 <yashupadhyay06oct@gmail.com>
2026-01-29 11:55:05 +0530

    comit

---
+++ jan-29/server.js
@@ -0,0 +1,16 @@
+// import the http module 
+const http=require("http");
+
+// create server
+const server=http.createServer((req,res)=>{
+    res.statusCode=200;
+    res.setHeader("Content-Type","text/plain");
+    res.end("welcome to the Node.js Tutorial");
+});
+
+// listen on port 3000
+server.listen(3000,()=>{
+    console.log(3000,()=>{
+        console.log("server is running on http://localhost:3000");
+    });
+});
